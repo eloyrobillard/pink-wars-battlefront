379b1599101fa232e0d2daf9a455ba8cb2bfbd9a
Eloy <eloy.robillard@gmail.com>
2021-11-10 19:25:13 +0100

    ✨ feat(nav bar): show main title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
 import Canvas from './Canvas';
 import './App.css';
 
+function NavBar() {
+  return (
+    <a id="nav-bar" href="https://fontmeme.com/star-wars-font/">
+      <img src="https://fontmeme.com/permalink/211110/50d0d2aa0f0cc86184324342f5bfc93e.png" alt="star-wars-font" />
+    </a>
+  )
+}
+
 function App() {
   
 
   return (
     <div className="App">
+      <NavBar />
       <Canvas />
     </div>
   );
