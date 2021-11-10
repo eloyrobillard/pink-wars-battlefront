8b2e135ff491ce399ba7e09676a60dbf7bdf6c48
Eloy <eloy.robillard@gmail.com>
2021-11-10 16:34:02 +0100

    🐞 fix(npm modules): missing babel-loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import Canvas from './Canvas';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Canvas />
       </header>
     </div>
   );
--- client/src/Canvas.tsx
+++ client/src/Canvas.tsx
@@ -1,24 +1,27 @@
-import Sketch from 'react-p5';
+import React from "react";
+import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
+
 
 export default function Canvas() {
   let x = 50;
 	const y = 50;
 
-  const setup = (p5: p5Types, canvasParentRef: Element) => {
-    // use parent to render the canvas in this ref
+	//See annotations in JS for more information
+	const setup = (p5: p5Types, canvasParentRef: Element) => {
+		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(500, 500).parent(canvasParentRef);
 	};
 
 	const draw = (p5: p5Types) => {
+		p5.background('#ffdbed');
+		p5.ellipse(x, y, 70, 70);
     // NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
-		p5.background(0);
-		p5.ellipse(x, y, 70, 70);
 		x++;
 	};
 
-  return <Sketch setup={setup} draw={draw} /> 
+	return <Sketch setup={setup} draw={draw} />;
 }
