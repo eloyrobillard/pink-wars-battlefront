68d673f1734ad059221f5bd7297446b05f11e603
Eloy <eloy.robillard@gmail.com>
2021-11-10 19:18:10 +0100

    🐞 fix(canvas): prevent scroll bars

--- client/src/Canvas.tsx
+++ client/src/Canvas.tsx
@@ -1,27 +1,23 @@
-import math from './math/index';
 import Game from './game-engine/index';
-import { Ship } from './types/index';
 import Sketch from 'react-p5';
 import p5Types from 'p5'; //Import this for typechecking and intellisense
 
 export default function Canvas () {
-	// let x = 500;
-	// const y = 50;
 
-	//See annotations in JS for more information
 	const setup = (p5: p5Types, canvasParentRef: Element) => {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
-		p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
+		// NOTE -1 on all width/height vals to prevent scroll bars
+		p5.createCanvas(window.innerWidth - 1, window.innerHeight - 1).parent(canvasParentRef);
 
 		// always fit canvas to window
-		window.addEventListener('resize', () => p5.resizeCanvas(window.innerWidth, window.innerHeight));
+		window.addEventListener('resize', () => p5.resizeCanvas(window.innerWidth - 1, window.innerHeight - 1));
 	};
 
+	// redraw canvas on every browser update
 	const draw = (p5: p5Types) => {
 		p5.background('#ffdbed');
 		p5.noStroke();
-		// p5.ellipse(x, y, 70, 70);
 		Game.pool.map((ship, i) => {
 			ship.addRot(1);
 			const { body } = ship;
@@ -34,8 +30,11 @@ export default function Canvas () {
 		// NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
-		// x++;
 	};
 
-	return <Sketch setup={setup} draw={draw} />;
+	return (
+		<div className=".Canvas">
+			<Sketch setup={setup} draw={draw} />
+		</div>
+	);
 }
