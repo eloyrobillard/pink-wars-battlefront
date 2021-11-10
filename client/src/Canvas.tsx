0937c7057a32f248ea272867cf5c1ef71d15a010
Eloy <eloy.robillard@gmail.com>
2021-11-10 16:53:55 +0100

    🌈 style(prettier): add .prettierrc

--- client/src/Canvas.tsx
+++ client/src/Canvas.tsx
@@ -1,9 +1,8 @@
-import Sketch from "react-p5";
-import p5Types from "p5"; //Import this for typechecking and intellisense
+import Sketch from 'react-p5';
+import p5Types from 'p5'; //Import this for typechecking and intellisense
 
-
-export default function Canvas() {
-  let x = 50;
+export default function Canvas () {
+	let x = 50;
 	const y = 50;
 
 	//See annotations in JS for more information
@@ -11,13 +10,14 @@ export default function Canvas() {
 		// use parent to render the canvas in this ref
 		// (without that p5 will render the canvas outside of your component)
 		p5.createCanvas(window.innerWidth, window.innerHeight).parent(canvasParentRef);
-    window.addEventListener('resize', () => p5.resizeCanvas(window.innerWidth, window.innerHeight));
+		window.addEventListener('resize', () => p5.resizeCanvas(window.innerWidth, window.innerHeight));
 	};
 
 	const draw = (p5: p5Types) => {
 		p5.background('#ffdbed');
 		p5.ellipse(x, y, 70, 70);
-    // NOTE: Do not use setState in the draw function or in functions that are executed
+		p5.triangle(30, 75, 58, 20, 86, 75);
+		// NOTE: Do not use setState in the draw function or in functions that are executed
 		// in the draw function...
 		// please use normal variables or class properties for these purposes
 		x++;
