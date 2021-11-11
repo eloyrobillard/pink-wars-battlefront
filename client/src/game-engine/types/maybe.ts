a00c6d081b7f6ea657a1ea17c4245f0476424bb8
Eloy <eloy.robillard@gmail.com>
2021-11-11 15:26:05 +0100

    ✨ feat(types): create Maybe type

---
+++ client/src/game-engine/types/maybe.ts
@@ -0,0 +1,15 @@
+export default class Maybe<T> {
+  constructor(private contents: T | null = null) {}
+
+  /**
+   * Returns value inside of Maybe, or provided default if Maybe is empty.
+   * @param  {T} def
+   * @returns T
+   */
+  unwrapOr(def: T): T {
+    if (this.contents === null) {
+      return def;
+    }
+    return this.contents;
+  }
+}
